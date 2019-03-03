807d167798f8f734da64ce0277e6bd8aa2ec96d5
Yumiya <tyumiya@deloitte.com>
2019-03-03 10:12:09 -0300

    Getting books from BookAPI

---
+++ src/BookList.js
@@ -0,0 +1,16 @@
+import React from "react"
+
+class BookList extends React.Component {
+    constructor() {
+        super()
+    }
+
+    render() {
+        return(
+            <div></div>
+        )
+    }
+
+}
+
+export default BookList
